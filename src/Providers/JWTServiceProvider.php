63a5910113db62b3793c510d165f130b657fd224
lee-to <lee-to@users.noreply.github.com>
2025-01-23 13:42:27 +0000

    Fix styling

--- src/Providers/JWTServiceProvider.php
+++ src/Providers/JWTServiceProvider.php
@@ -16,7 +16,7 @@ final class JWTServiceProvider extends ServiceProvider
             __DIR__ . '/../../config/moonshine-jwt.php' => config_path('moonshine-jwt.php'),
         ]);
 
-        $this->app->bind(JWTContract::class, fn(): JWT => new JWT(
+        $this->app->bind(JWTContract::class, fn (): JWT => new JWT(
             config('moonshine-jwt.secret')
         ));
     }
